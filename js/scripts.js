f707f001747a906b3e73cc32273033b2aeac4c6c
Willejka <ewasinska@gmail.com>
2018-02-03 22:13:27 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//scripts.js
+var withButton = document.getElementsByClassName('button');
+console.log(withButton);
+
+var i = 0;
+
+for ( var i=0; i <= withButton.length; i++ ) {
+alert( withButton[i].innerText);
+console.log(withButton[i].innerText);
+};
